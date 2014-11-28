4ba4cf10a9913011ba4a1ec8df8ad52d8ee1c00e
Krazh <moped47@gmail.com>
2014-11-28 23:38:54 +0100

    Diverse forbedringer. Bla dynamisk UserControl(som virkelig var nødvendig)

--- Roskilde Turist v4/Roskilde Turist v4/ViewModel/ButikkerViewModel.cs
+++ Roskilde Turist v4/Roskilde Turist v4/ViewModel/ButikkerViewModel.cs
@@ -10,36 +10,39 @@ namespace Roskilde_Turist_v4.ViewModel
     using Model;
     class ButikkerViewModel
     {
-        public static ObservableCollection<Butikker> ButikkerCollection { get; set; }
-        public static List<Butikker> ButikKatalog;
-        public string[] _aabningstider = new string[] { "Mandag-Fredag: 10.00 - 18.00", "Lørdag: 10.00 - 18.00" };
+        public static ObservableCollection<Butikker> Collection { get; set; }
+        public static List<Butikker> Katalog;
+        private readonly string[] _aabningstider = { "Mandag-Fredag: 10.00 - 18.00", "Lørdag: 10.00 - 18.00" };
 
 
         public ButikkerViewModel()
         {
-            ButikKatalog = new List<Butikker>();
+            Katalog = new List<Butikker>();
             SetButik();
-            ButikkerCollection = new ObservableCollection<Butikker>();
+            Collection = new ObservableCollection<Butikker>();
             UpdateListe();
         }
 
         public void SetButik()
         {
-            ButikKatalog.Add(new Butikker("Ro's Torv", "Elektronik", 1, "Fona", "22334455", _aabningstider));
-            ButikKatalog.Add(new Butikker("Ro's Torv", "Elektronik", 2, "GameStop", "22334455", _aabningstider));
-            ButikKatalog.Add(new Butikker("Algade 28", "Elektronik", 3, "3 Butik", "33333333", _aabningstider));
-            ButikKatalog.Add(new Butikker("Stationen", "Kiosk", 4, "7-Eleven", "07110711", _aabningstider));
+            Katalog.Add(new Butikker("Ro's Torv", "Elektronik", 1, "Fona", "22334455", _aabningstider));
+            Katalog.Add(new Butikker("Ro's Torv", "Elektronik", 2, "GameStop", "22334455", _aabningstider));
+            Katalog.Add(new Butikker("Algade 28", "Elektronik", 3, "3 Butik", "33333333", _aabningstider));
+            Katalog.Add(new Butikker("Stationen", "Kiosk", 4, "7-Eleven", "07110711", _aabningstider));
+            Katalog.Add(new Butikker("Algade 36", "Tøj", 5, "Oda's Minde", "46351920", _aabningstider));
+            Katalog.Add(new Butikker("Allehelgensgade 7 st", "Tøj", 6, "ID Lingeri", "46366301", _aabningstider));
+
         }
 
         public void UpdateListe()
         {
             var tempButikListe =
-                from butik in ButikKatalog
+                from butik in Katalog
                 where butik.Test
                 select butik;
 
             foreach (Butikker butik in tempButikListe)
-                ButikkerCollection.Add(new Butikker(butik.Adresse, butik.Kategori, butik.Id, butik.Navn, butik.Tlf, butik.Aabningstider));
+                Collection.Add(new Butikker(butik.Adresse, butik.Kategori, butik.Id, butik.Navn, butik.Tlf, butik.Aabningstider));
             
         }
     }
